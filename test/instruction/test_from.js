8e0f9dde7133c6da90eb681df3953d7796cc91a0
gustavderdrache <aford@forumone.com>
2019-06-05 13:45:56 -0400

    adds support for AS

--- test/instruction/test_from.js
+++ test/instruction/test_from.js
@@ -5,13 +5,15 @@ var registry = 'registry'
 var digest = 'digest'
 var image = 'image'
 var tag = 'tag'
+var stage = 'stage'
 
-function opts(registry, image, digest, tag) {
+function opts(registry, image, digest, tag, stage) {
   var o = {}
   if(registry) o.registry = registry
   if(image) o.image = image
   if(digest) o.digest = digest
   if(tag) o.tag = tag
+  if(stage) o.stage = stage
   return o
 }
 
@@ -33,7 +35,29 @@ describe('testing instruction "FROM"', () => {
       var expected_result = expected[i]
       it('shoud make '+expected_result, ()=> {
         assert.equal(result, expected_result)
-      })    
+      })
+    }
+  })
+
+  describe('stages', () => {
+    const inputs = [
+      opts(false, image, false, false, stage),
+      opts(false, image, digest, false, stage),
+      opts(false, image, false, tag, stage),
+    ]
+
+    const expected = [
+      'FROM image AS stage',
+      'FROM image@digest AS stage',
+      'FROM image:tag AS stage'
+    ]
+
+    for (var i = 0; i < inputs.length; ++i) {
+      var result = ins.from(inputs[i])
+      var expected_result = expected[i]
+      it('should make '+expected_result, ()=> {
+        assert.equal(result, expected_result)
+      })
     }
   })
 
@@ -42,7 +66,7 @@ describe('testing instruction "FROM"', () => {
     it('should just prepend "FROM "', ()=> {
       var result = ins.from("just_prepends_from")
       assert.deepEqual(result, expected)
-    })    
+    })
   })
 
   describe('with invalid input', () => {
@@ -68,7 +92,7 @@ describe('testing instruction "FROM"', () => {
     const expected = ''
     it('', ()=> {
       assert.deepEqual(result, expected)
-    })    
+    })
   })  */
 
 })
